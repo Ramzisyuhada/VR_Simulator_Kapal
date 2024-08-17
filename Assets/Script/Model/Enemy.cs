2c3fd87c1d785c36276c9682b36abb590dfdca73
Muhammad Fadlansyah Zikri <ramzi.syuhada@mhs.itenas.ac.id>
2024-08-17 20:29:17 +0700

    First Commit

---
+++ Assets/Script/Model/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy 
+{
+
+    private float Health;
+    private float Damage;
+
+    public Enemy(float Health, float Damage)
+    {
+        this.Health = Health;
+        this.Damage = Damage;
+    }
+
+    public float GetHealth()
+    {
+        return this.Health;
+    }
+
+    public void SetHealth(float Health)
+    {
+        this.Health = Health;
+    }
+
+
+    public float GetDamage()
+    {
+        return this.Damage;
+
+    }
+
+    public void SetDamage(float Damage)
+    {
+        this.Damage = Damage;
+    }
+}
---
+++ Assets/Script/View/ViewPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewPlayer : MonoBehaviour
+{
+    [Header("GameObject")]
+    [SerializeField] private GameObject Bullets;
+    [Header("GUI")]
+    [SerializeField] private Text Health;
+    [SerializeField] private 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
